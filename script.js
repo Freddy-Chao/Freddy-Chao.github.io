503b8e9c561d216ca2234516861b036c57f69db1
Freddy-Chao <freddychao613@gmail.com>
2022-01-20 22:27:34 -0800

    first commit

---
+++ script.js
@@ -0,0 +1,109 @@
+const TheSongButton = document.getElementById('TheSong');
+
+TheSongButton.addEventListener('click', () => {
+  alert('Correct! Add 1.5 points to your total!');
+});
+const TheTang = document.getElementById('TheTang');
+TheTang.addEventListener('click', () => {
+  alert('Incorrect! Subtract 0.5 points from your total and do 2 push-ups!');
+});
+const TheHan = document.getElementById('TheHan');
+TheHan.addEventListener('click', () => {
+  alert('Incorrect! Subtract 0.5 points and do 6 jumping-jacks! ');
+});
+const Confucianism = document.getElementById('Confucianism');
+Confucianism.addEventListener('click', () => {
+  alert('Correct! Add 1.5 points to your total!');
+});
+const TheTangDynasty = document.getElementById('TheTangDynasty');
+TheTangDynasty.addEventListener('click', () => {
+  alert('Incorrect! Subtract 1 point to your total and do 8 jumping jacks!');
+});
+const TheHanDynsasty = document.getElementById('TheHanDynasty');
+TheHanDynsasty.addEventListener('click', () => {
+  alert('Correct! Add 3 points to your total!');
+});
+const TheSongDynasty = document.getElementById('TheSongDynasty');
+TheSongDynasty.addEventListener('click', () => {
+  alert('Incorrect! Subtract 1 point from your total and do 5 jumping jacks!');
+});
+const TheSuiDynasty = document.getElementById('TheSuiDynasty');
+TheSuiDynasty.addEventListener('click', () => {
+  alert('Incorrect! Subtract 1 point from your total and do 6 jumping-jacks!');
+});
+const Gunpowder = document.getElementById('Gunpowder');
+Gunpowder.addEventListener('click', () => {
+  alert('Correct! Add 2.5 points to your total!');
+});
+const Salt = document.getElementById('Salt');
+Salt.addEventListener('click', () => {
+  alert('Incorrect! Subtract 1.5 point from your total!');
+});
+const WoodBlock = document.getElementById('WoodBlock');
+WoodBlock.addEventListener('click', () => {
+  alert('Wrong! Subtract 1 point from your total!');
+});
+const Paper = document.getElementById('Paper');
+Paper.addEventListener('click', () => {
+  alert('Nice Try, Subtract 1 point from your total!');
+});
+const Inlets = document.getElementById('Inlets');
+Inlets.addEventListener('click', () => {
+  alert('Incorrect! Subtract 1.5 points from your total!');
+});
+const Terraces = document.getElementById('Terraces');
+Terraces.addEventListener('click', () => {
+  alert('Correct! Add 5.5 points to your total!');
+});
+const Temples = document.getElementById('Temples');
+Temples.addEventListener('click', () => {
+  alert('Incorrect! Subtract 1.5 points from your total!');
+});
+const Shovels = document.getElementById('Shovels');
+Shovels.addEventListener('click', () => {
+  alert('Incorrect! Subtract 1.5 points from your total!');
+});
+const Twenty = document.getElementById('years');
+years.addEventListener('click', () => {
+  alert('Wrong! Subtract 1.5 points from your total!');
+});
+const year = document.getElementById('year');
+year.addEventListener('click', () => {
+  alert('Correct! Add 5.5 points to your total!');
+});
+const m = document.getElementById('m');
+m.addEventListener('click', () => {
+  alert('Incorrect! Subtract 1.5 points from your total!');
+});
+const cat = document.getElementById('cat');
+cat.addEventListener('click', () => {
+  alert('Incorrect! Subtract 1.5 points from your total!');
+});
+const Academics = document.getElementById('Academics');
+Academics.addEventListener('click', () => {
+  alert('Incorrect! Subtract 2.5 points from your total!');
+});
+const Political = document.getElementById('Political_Knowledge');
+Political.addEventListener('click', () => {
+  alert('Incorrect! Subtract 2.5 points from your total!');
+});
+const History = document.getElementById ('chicken');
+chicken.addEventListener ('click',()=>{alert('Incorrect! Subtract 2.5 points from your total!')})
+const Broccoli = document.getElementById ('Broccoli');
+Broccoli.addEventListener('click',()=>{alert('Correct! Add 10.5 points to your total!')})
+const one = document.getElementById ('one');
+one.addEventListener ('click',()=>{alert('Incorrect! Subtract 2.5 points from your total!')})
+const two = document.getElementById ('two');
+two.addEventListener ('click',()=>{alert ('Correct! Add 13 points to your total!')})
+const three = document.getElementById ('three');
+three.addEventListener ('click',()=>{alert('Incorrect! Subtract 2.5 points from your total!')})
+const four = document.getElementById ('four');
+four.addEventListener ('click',()=>{alert('Wrong! Subtract 2.5 points from your total!')})
+const Wall = document.getElementById('Wall');
+Wall.addEventListener ('click',()=>{alert('Incorrect! Subtract 3 points from your total!')})
+const City = document.getElementById('City');
+City.addEventListener ('click',()=>{alert('Wrong! Subtract 3 points from your total! ')})
+const Sailor = document.getElementById ('Sailor');
+Sailor.addEventListener ('click',()=>{alert('Perfect! Add 15 points to your total!')})
+const Emperor = document.getElementById ('Emperor');
+Emperor.addEventListener ('click',()=>{alert('Incorrect! Subtract 3 points from your total!')})
